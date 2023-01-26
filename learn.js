1f982ec97594241fcef5efbc4368fcae144dc6af
Heba Omar <97067717+hebaomar94@users.noreply.github.com>
2023-01-26 16:52:14 +0200

    Create learn.js

---
+++ learn.js
@@ -0,0 +1,9 @@
+let newName = "Ann";
+console.log("my name is" + " " + newName);
+//new declare
+newName = "BEB";
+console.log("my name is" + " " + newName);
+
+//find datatype
+
+console.log(typeof newName);
